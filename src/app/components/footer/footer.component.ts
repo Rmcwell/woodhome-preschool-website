b3658bad3892a3c5f2fe84eb5685cd5d9a77cf8b
Riley <rmcaldwell2015@gmail.com>
2025-08-09 00:01:57 -0400

    contact form

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,14 @@
 import { Component  } from '@angular/core';
 import { LucideIconsModule } from '../../lucide.module';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer-base',
   standalone: true,
-  imports: [LucideIconsModule],
+  imports: [
+    LucideIconsModule,
+    RouterModule
+  ],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
